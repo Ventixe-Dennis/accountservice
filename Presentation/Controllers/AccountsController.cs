07fc8ce9acbacd26a77e259e072c93bdb0b5f764
Dennis Hed Rudin <Dennishr1994@hotmail.com>
2025-05-16 15:13:36 +0200

    Add project files.

---
+++ Presentation/Controllers/AccountsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Presentation.Services;
+
+namespace Presentation.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountsController(IAccountSerivce accountService) : ControllerBase
+{
+    private readonly IAccountSerivce _accountService = accountService;
+}
---
+++ Presentation/Services/AccountService.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Presentation.Services;
+
+public class AccountService(UserManager<IdentityUser> userManager) : IAccountSerivce
+{
+    private readonly UserManager<IdentityUser> _userManager = userManager;
+}
